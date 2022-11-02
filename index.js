622858c99816d8da5a2cbe1ea7d4dd0d1e456845
dstuden <studen.david@gmail.com>
2022-11-02 17:41:05 +0100

    initial design

---
+++ index.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  let downArrow = document.getElementsByClassName("down-arrow")[0];
+  downArrow.style.animation = "showArrow 0.3s ease";
+  downArrow.style.opacity = 1.0;
+}, 1000);
